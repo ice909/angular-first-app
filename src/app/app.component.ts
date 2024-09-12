2f00e331672f7c281bf0f1dd99487f341d001acd
ice909 <tonimayloneya@gmail.com>
2024-09-12 11:35:12 +0800

    feat: create home page
    创建首页

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `<h1>Hello World</h1>`,
+  imports: [RouterOutlet, HomeComponent],
+  templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
