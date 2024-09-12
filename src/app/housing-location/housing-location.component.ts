aef80e8b92685261ad90fa25d8b584c7751421b7
ice909 <tonimayloneya@gmail.com>
2024-09-12 11:47:22 +0800

    feat: render housing location
    渲染房屋位置

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HousingLocation } from './../housinglocation';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-housing-location',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './housing-location.component.css'
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HousingLocation
 }
