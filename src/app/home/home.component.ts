f5f5344eb24fc4c3677992d2aa6cd5e6c696427c
ice909 <tonimayloneya@gmail.com>
2024-09-15 15:47:56 +0800

    feat: impl search feature
    实现搜索功能

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,10 +13,21 @@ import { HousingService } from '../housing.service';
 export class HomeComponent {
   readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
   housingLocationList: HousingLocation[] = []
-
+  filteredLocationList: HousingLocation[] = [];
   housingService = inject(HousingService)
 
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations()
+    this.filteredLocationList = this.housingLocationList
+  }
+
+  filterResult(text: string) {
+    console.log(text)
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList
+      return
+    }
+    this.filteredLocationList = this.housingLocationList.filter((housingLocation) => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    )
   }
 }
