f93c5b5bf5ae13958b2663431138fe8d91fd5d8f
ice909 <tonimayloneya@gmail.com>
2024-09-15 15:05:03 +0800

    feat: add router and detail page
    添加路由和详情页

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DetailComponent } from './detail/detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: '首页'
+  },
+  {
+    path: 'details/:id',
+    component: DetailComponent,
+    title: '详情'
+  }
+];
